f85b9fbddd64d475ee7a8c9eda16a86fe7ba7237
Aydima <basyada_holonda@mail.ru>
2023-04-05 13:11:07 +0300

    Объединение

--- frontend/src/features/Main/Main.tsx
+++ frontend/src/features/Main/Main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import Contacts from '../contacts/Contacts';
 import ScoringGroups from '../ScoringGroups/ScoringGroups';
 import RaceList from './RaceList';
+import TimeBeforeRace from './TimeBeforeRace';
 
 function Main(): JSX.Element {
   return (
     <>
+      <TimeBeforeRace />
       <RaceList />
       <ScoringGroups />
+      <Contacts />
+      {/* <p></p> */}
     </>
   );
 }
